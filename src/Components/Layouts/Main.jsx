4aef3d5454ba94a08c6f667ac9eb81aa505777be
vatiza <rakibur.mac@gmail.com>
2024-05-04 03:05:10 +0600

    first commit

---
+++ src/Components/Layouts/Main.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../Pages/Navbar/Navbar";
+import { Outlet } from "react-router-dom";
+
+const Main = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+    </div>
+  );
+};
+
+export default Main;
---
+++ src/Components/Providers/AuthProviders.jsx
@@ -0,0 +1,16 @@
+
+import { createContext, useState } from "react";
+
+export const AuthContext = createContext();
+const AuthProviders = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  const authInfo = {
+    user,
+  };
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProviders;
